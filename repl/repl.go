d7c7ee9c7a4f1034827e37ae94c665931c57cbc6
tonoy30 <tonoy.sust@gmail.com>
2021-11-27 02:05:05 +0600

    repl exit and help command added

--- repl/repl.go
+++ repl/repl.go
@@ -14,15 +14,23 @@ func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
-		fmt.Fprintf(out, PROMPT)
+		fmt.Fprintf(out, "%v", PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
 		line := scanner.Text()
-		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		switch line {
+		case "exit":
+			return
+		case "help":
+			fmt.Fprintln(out, "Commands:")
+		default:
+			l := lexer.New(line)
+			for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+				fmt.Fprintf(out, "%+v\n", tok)
+			}
 		}
+
 	}
 }
