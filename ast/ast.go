a6acd078a885650e290bb4200b498834f8dc890e
tonoy30 <tonoy.sust@gmail.com>
2021-11-27 03:40:33 +0600

    ast preparation done

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "mortar/token"
+import (
+	"bytes"
+	"mortar/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -29,6 +33,10 @@ func (i *Identifier) expressionNode() {
 
 }
 func (i *Identifier) TokenLiteral() string {
+	return i.Value
+}
+
+func (i *Identifier) String() string {
 	return i.Token.Literal
 }
 
@@ -38,11 +46,23 @@ type LetStatement struct {
 	Value Expression
 }
 
-func (i *LetStatement) statementNode() {
+func (ls *LetStatement) statementNode() {
 
 }
-func (i *LetStatement) TokenLiteral() string {
-	return i.Token.Literal
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
 }
 
 type ReturnStatement struct {
@@ -57,10 +77,44 @@ func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
 }
 
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return rs.Token.Literal
+}
+
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (e *ExpressionStatement) statementNode() {
+
+}
+func (e *ExpressionStatement) TokenLiteral() string {
+	return e.Token.Literal
+}
+func (e *ExpressionStatement) String() string {
+	if e.Expression != nil {
+		return e.Expression.String()
+	}
+	return ""
+}
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
 	} else {
 		return ""
 	}
+}
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
 }
